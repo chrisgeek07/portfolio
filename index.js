bdbdf76e82b98b607789bc911369509bf21548e9
Christian BeauxYeux <gildweek@gmail.com>
2021-12-03 13:51:14 +0400

    Completed page animations for now

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 gsap.from('#intro', {opacity: 0, duration: 1, y: -100, ease: 'Power1.easeInOut'});
 gsap.from('.main-header', {opacity: 0, duration: 1, y: -500, ease: 'power4.out', delay: 0.5});
 gsap.from('.anim1', {opacity: 0, duration: 1, y: -30, ease: 'Power1.easeInOut', delay: 0.8, stagger: 0.6});
+
+gsap.to('.head-proj', {
+    scrollTrigger: {
+        trigger: '.head-proj',
+        start: 'bottom center'
+    },
+    scale: 1.5,
+    opacity: 1
+})
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.show-card-container',
+    }
+});
+
+tl.from('.show-card', { y: -100, opacity: 0, duration: 1, ease: 'power4.out', stagger: 0.6})
