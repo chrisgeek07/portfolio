44418f1958d9dc5c9f9fef41da8b9dc392ffc878
Christian BeauxYeux <gildweek@gmail.com>
2021-12-03 13:11:59 +0400

    Added scrollTrigger cdn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-gsap.from('#intro', {opacity: 0, duration: 1, y: -100, ease: 'Power2.easeInOut'});
-gsap.from('.main-header', {opacity: 0, duration: 1, y: -50, ease: 'Power2.easeInOut', delay: 0.5});
-gsap.from('#avatar', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1});
-gsap.from('.intro-txt', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1.1});
-gsap.from('.btn-blue', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1.5});
+gsap.from('#intro', {opacity: 0, duration: 1, y: -100, ease: 'Power1.easeInOut'});
+gsap.from('.main-header', {opacity: 0, duration: 1, y: -500, ease: 'power4.out', delay: 0.5});
+gsap.from('.anim1', {opacity: 0, duration: 1, y: -30, ease: 'Power1.easeInOut', delay: 0.8, stagger: 0.6});
