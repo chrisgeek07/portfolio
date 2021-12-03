5bd9b9ee4c2ee28297d07cbe251db4fb2cd8c08a
Christian BeauxYeux <gildweek@gmail.com>
2021-12-03 12:50:48 +0400

    Added basic JS animation with Gsap

---
+++ index.js
@@ -0,0 +1,5 @@
+gsap.from('#intro', {opacity: 0, duration: 1, y: -100, ease: 'Power2.easeInOut'});
+gsap.from('.main-header', {opacity: 0, duration: 1, y: -50, ease: 'Power2.easeInOut', delay: 0.5});
+gsap.from('#avatar', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1});
+gsap.from('.intro-txt', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1.1});
+gsap.from('.btn-blue', {opacity: 0, duration: 1, y: -50, ease: 'Power3.easeInOut', delay: 1.5});
